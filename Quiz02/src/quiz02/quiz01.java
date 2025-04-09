503220a6d4ece82bcb159645123a82881e2af0ba
DanGitUser <imtaehoonkim@gmail.com>
2025-04-09 23:46:32 +0900

    adding files

--- Quiz02/src/quiz02/quiz01.java
+++ Quiz02/src/quiz02/quiz01.java
@@ -1,5 +1,15 @@
 package quiz02;
 
 public class quiz01 {
-
+	public static void main(String[] args) {
+		
+		int colorPen = 5 * 12;
+		int studentCount = 27;
+		
+		int divColorpen = colorPen / studentCount;
+		System.out.println("Number of crayons shared per student :"+divColorpen);
+		
+		int remainColorPen = colorPen % studentCount;
+		System.out.println("Remaining crayons :"+remainColorPen);
+	}
 }
