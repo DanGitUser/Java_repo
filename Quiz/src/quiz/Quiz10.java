198e14bae3d0aac477bcae4986f219d4f6cd60f2
DanGitUser <imtaehoonkim@gmail.com>
2025-04-09 10:21:16 +0900

    commit

--- Quiz/src/quiz/Quiz10.java
+++ Quiz/src/quiz/Quiz10.java
@@ -2,15 +2,17 @@ package quiz;
 
 public class Quiz10 {
 	public static void main(String[] args) {
-		[문제 10] 입장 심사 시스템 구성
-		어떤 공연장에서는 다음 조건을 모두 만족해야 입장이 가능하다.
-		나이가 18세 이상이고
-		티켓을 가지고 있으며
-		VIP가 아닐 경우만 입장 가능
-		다음 변수를 사용하여 입장 가능 여부(canEnter)를 판단하는 코드를 작성하시오.
+//		[문제 10] 입장 심사 시스템 구성
+//		어떤 공연장에서는 다음 조건을 모두 만족해야 입장이 가능하다.
+//		나이가 18세 이상이고
+//		티켓을 가지고 있으며
+//		VIP가 아닐 경우만 입장 가능
+//		다음 변수를 사용하여 입장 가능 여부(canEnter)를 판단하는 코드를 작성하시오.
 
 		int age = 20;
 		boolean hasTicket = true;
-		boolean isVIP = false;*/
+		boolean isVIP = false;
+		boolean canEnter = age >= 18 && !isVIP && hasTicket;
+		System.out.println("Entry permit : " +canEnter);
 	}
 }
