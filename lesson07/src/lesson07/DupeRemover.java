bc87a342410634d54da79be871767e3dbf0345c6
DanGitUser <imtaehoonkim@gmail.com>
2025-04-15 00:29:26 +0900

    adding files

--- lesson07/src/lesson07/DupeRemover.java
+++ lesson07/src/lesson07/DupeRemover.java
@@ -7,11 +7,27 @@ import java.util.Arrays;
 public class DupeRemover {
 	public static void main(String[] args) {
 		
-		int[] ranNum = new int[50];
-		int idx = 0;
-		while(true) { //infinite loop if there is no break.
-			int number = (int)(Math.random() * 20 + 1) ;
-			
-		}
-	}
+        int[] nums = new int[50];
+        int[] uniq = new int[50];
+        int uCount = 0;
+
+        for (int i = 0; i < 50; i++) {
+            int num = (int) (Math.random() * 20) + 1;
+            nums[i] = num;
+
+            boolean dupe = false;
+            for (int j = 0; j < uCount; j++) {
+                if (num == uniq[j]) {
+                    dupe = true;
+                    break;
+                }
+            }
+            if (!dupe) {
+                uniq[uCount++] = num;
+            }
+        }
+
+        System.out.println("Random numbers: " + Arrays.toString(nums));
+        System.out.println("Unique numbers: " + Arrays.toString(Arrays.copyOf(uniq, uCount)));
+    }
 }
